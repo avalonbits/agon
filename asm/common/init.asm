f5506d2332ac8061fe07b1e7e284c3599e3ad0e9
Igor Cananea <icc@avalonbits.com>
2023-02-03 17:04:35 -0500

    Working hello world version.

--- asm/common/init.asm
+++ asm/common/init.asm
@@ -1,19 +1,36 @@
-    .ASSUME ADL = 1 ; Start in ADL mode
+;
+; Initialization for an eZ80 assembler program that works on the Agon MOS.
+;
+
+            .ORG    040000h     ; All eZ80 programs start at 0x040000
+            .ASSUME ADL = 1     ; Start in ADL mode
 
 
-    JP  _START
+            JP  _START          ; Jump over the obligatory header.
 
-    ;
-    ; The header stuff is from byte 64 onwards
-    ;
-    .BLOCK  60
-    .db     "MOS"   ; Flag for MOS - to confirm this is a valid MOS command
-    .db     00h     ; MOS header version 0
-    .db     01h     ; Flag for run mode (0: Z80, 1: ADL)
+            ; The required MOS header.
+            ;
+            .BLOCK  60
+            .db     "MOS"   ; Flag for MOS - to confirm this is a valid MOS command
+            .db     00h     ; MOS header version 0
+            .db     01h     ; Flag for run mode (0: Z80, 1: ADL)
 
 ;
 ; And the code follows on immediately after the header
+; The return code for the application must be set on register HL.
 ;
-_START:
-    EI              ; Enable the MOS interrupts
-    JP      main    ; Start user code
+_START:     PUSH    AF      ; Preserve registers, except HL.
+            PUSH    BC
+            PUSH    DE
+            PUSH    IX
+            PUSH    IY
+
+            CALL    main    ; Start user code
+
+            POP     IY      ; Restore registers
+            POP     IX
+            POP     DE
+            POP     BC
+            POP     AF
+
+            RET             ; We're done.
--- asm/helloworld/helloworld.asm
+++ asm/helloworld/helloworld.asm
@@ -1,25 +1,26 @@
-	.ASSUME ADL = 1
+            .ASSUME ADL = 1
 
-	#include "../common/init.asm"
+            #include "../common/init.asm"
 
 ; The main routine
 ;
-main:
-	LD      HL, hello_world
-	CALL    prstr
-	LD      HL, 0
-	RET.L
+main:       LD      HL, hello_world     ; Load string adddres to HL register
+            CALL    prstr               ; Call print string funcition
+            LD      HL, 0               ; Set application return code to 0
+            RET                         ; Done.
 
-; Print a zero-terminated string
+; Print a zero-terminated strin
+; Arguments:
+;   - HL: pointer to start of zero-terminated string.
 ;
-prstr:
-	LD      A,(HL)
-	OR      A
-	RET     Z
-	RST.LIL 10h
-	INC     HL
-	JR      prstr
+prstr:      LD      A,(HL)              ; Load curreent character from string
+            OR      A                   ; Check if it is zero
+            RET     Z                   ; If zero, we are done.
+
+            RST.LIL 10h                 ; Send the charcter to VDP
+            INC     HL                  ; Get the next charcter
+            JR      prstr               ; Loop back.
 
 ; Sample text
 ;
-hello_world: .db "Hello World\n\r",0
+hello_world:    .db "Hello World\n\r",0
