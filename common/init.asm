e0271507bb65483610560139b6a78e9c12b0e522
Igor Cananea <icc@avalonbits.com>
2023-02-02 20:40:31 -0500

    Created hello world for agon light

---
+++ common/init.asm
@@ -0,0 +1,19 @@
+	.ASSUME ADL = 1 ; Start in ADL mode
+
+
+	JP _START
+
+	;
+	; The header stuff is from byte 64 onwards
+	;
+	.BLOCK 60
+	.db "MOS" ; Flag for MOS - to confirm this is a valid MOS command
+	.db 00h ; MOS header version 0
+	.db 01h ; Flag for run mode (0: Z80, 1: ADL)
+
+;
+; And the code follows on immediately after the header
+;
+_START:
+	EI ; Enable the MOS interrupts
+	JP main ; Start user code
---
+++ helloworld/helloworld.asm
@@ -0,0 +1,25 @@
+	.ASSUME ADL = 1
+
+	#include "../common/init.asm"
+
+; The main routine
+;
+main:
+	LD HL, hello_world
+	CALL prstr
+	LD HL, 0
+	RET.L
+
+; Print a zero-terminated string
+;
+prstr:
+	LD A,(HL)
+	OR A
+	RET Z
+	RST.LIL 10h
+	INC HL
+	JR prstr
+
+; Sample text
+;
+hello_world: .db "Hello World\n\r",0
